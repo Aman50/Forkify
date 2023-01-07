463a747b69b345a76553533cd8f0eb0c7270e4ea
Aman50 <amanbhalla50@gmail.com>
2023-01-07 22:36:25 +0530

    Added config and helpers

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const REQUEST_TIMOUT_SEC = 10;
--- src/js/model.js
+++ src/js/model.js
@@ -1,33 +1,17 @@
+import * as config from './config.js';
+import * as helper from './helpers.js';
+
 export const model = {
-  recipe: {}
+  recipe: {},
+  search: [],
+  bookmarks: []
 };
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-const submitGetRequest = async function (url, timeoutDuration) {
-  try {
-    const res = await Promise.race([fetch(url), timeout(timeoutDuration)]);
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(`${data.message} (${res.status})`);
-    }
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
 
 export const fetchRecipe = async function (recipeId) {
   try {
-    const res = await submitGetRequest(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`,
+    const res = await helper.submitGetRequest(
+      `${config.API_URL}${recipeId}`,
       10
     );
     const { recipe } = res.data;
